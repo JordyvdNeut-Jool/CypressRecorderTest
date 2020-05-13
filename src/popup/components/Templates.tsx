e1314812f8f562115ae7813dc6cf53ec52f6fc42
JordyvdNeut <jordyneut@hotmail.com>
2020-05-13 14:31:15 +0200

    Template function with node server bug

--- src/popup/components/Templates.tsx
+++ src/popup/components/Templates.tsx
@@ -1,56 +1,47 @@
 import * as React from "react";
+import { useEffect } from "react";
+import TemplateBlock from "./TemplateBlock";
 
 const chr = chrome.extension.getBackgroundPage();
 
 export default () => {
-
   const nodeServerPost = async (link, options) => {
     const response = await fetch("http://localhost:3000/" + link, options);
     const jsonResponse = await response.json();
     return jsonResponse;
   };
 
-  const readTest = async (): Promise<any[]> => {
+  const readTests = async (): Promise<any[]> => {
     const options = {
-      method: "POST",
+      method: "GET",
       headers: {
-        "Content-type": "application/json; charset=utf-8"
-      }
+        "Content-type": "application/json; charset=utf-8",
+      },
     };
-    var response = nodeServerPost("readTests", options);
-
-    var fileName = response.then(result => {
-      let values = Object.values(result);
-      return values;
+    var response = await nodeServerPost("readTests", options);
+    var files = await response.then(async (result) => {
+      chr.console.log("result: ", result);
+      return Object.values(result);
     });
-    return fileName;
+    chr.console.log("files: ", files);
+    return files;
   };
-  const pushTemplateName = () => {
-      readTest().then(result => {
-        result.forEach(element => {
-          element.forEach(fileName => {
-            var fileNameRow = "<button onClick={handleClick}>" + fileName + "</button>";
-            document.getElementById("test").innerHTML += fileNameRow;
-          });
+
+  const templateNames = async () => {
+    await readTests().then((result) => {
+      result.map((files: any[]) => {
+        files.map((template: string) => {
+          <TemplateBlock name={template} />;
         });
       });
-  };
-
-  const handleClick = () => {
-    // try {
-      var codeRow = "<textarea>Text</textarea>";
-      document.getElementById("code").innerHTML += codeRow;
-          // } catch (error) {
-      // throw new Error(error);
-    // }
+    });
   };
 
   return (
     <div id="templatesbox">
       <h1>Templates</h1>
-      {pushTemplateName()}
-      <div id="test"></div>
-      <div id="code"></div>
+      <TemplateBlock name={"template"} />
+      <ul id="template-display">{templateNames}</ul>
       <br />
     </div>
   );
